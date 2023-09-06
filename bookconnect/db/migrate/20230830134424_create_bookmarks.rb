cc929168ccfc582da766a375bdbe39bd9fb772c9
koto8 <tokoya0416@gmail.com>
2023-09-06 22:11:57 +0900

    新規リポジトリ登録前のコミット

--- bookconnect/db/migrate/20230830134424_create_bookmarks.rb
+++ bookconnect/db/migrate/20230830134424_create_bookmarks.rb
@@ -1,7 +1,9 @@
 class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
-
+      t.integer :BookmarkNo., null: false
+      t.integer :UserNo., null: false
+      t.integer :BookshelfNo., null: false
       t.timestamps
     end
   end
